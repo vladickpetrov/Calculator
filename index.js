f21aefe7e62d1ee8e358c82196ca0dae9f03c477
vladickpetrov <minnesota777@mail.ru>
2022-09-08 10:46:07 +0300

    bring js logic to buttons

--- index.js
+++ index.js
@@ -1,10 +1,93 @@
 const result = document.querySelector('.calculator__result');
-const buttonSticky = document.querySelectorAll('.sticky');
+const buttons = document.querySelectorAll('.calculator__button');
+const stickyButtons = document.querySelectorAll('.sticky');
+const deleteButton = document.querySelector('.ac');
+const posNegButton = document.querySelector('.change');
+const divideButton = document.querySelector('.divide');
+const procentButton = document.querySelector('.procent');
+const multyButton = document.querySelector('.multiply');
+const minButton = document.querySelector('.minus');
+const plusButton = document.querySelector('.plus');
+const isButton = document.querySelector('.is');
+const pointButton = document.querySelector('.point');
+let a;
+let b;
 
-buttonSticky.forEach((item) => {
-    item.addEventListener('click', () => {
-        item.classList.toggle('calculator__button_pressed');
-    })
-});
+function multiply() {
+    return a * b
+}
 
-window.navigator.vibrate(20000);
+function subtract() {
+    return a - b
+}
+
+function sum() {
+    return a + b
+}
+
+function divide() {
+    return a / b;
+}
+
+function procent() {
+    return (a / 100).toFixed(2);
+}
+
+function addEventListeners(callback) {
+    function putResultB(){
+        b = +result.textContent;
+        result.textContent = '';    
+        stickyButtons.forEach(button => {
+            button.removeEventListener('click', putResultB);
+        })
+        result.textContent = (`${callback()}`);
+    }
+    a = +result.textContent;
+    result.textContent = '';
+    stickyButtons.forEach(button => {
+        button.addEventListener('click', putResultB);
+    });
+}
+
+function checkButtonNumber(button) {
+    if(+button.textContent || +button.textContent == 0) {
+        button.addEventListener('click', () => {
+            result.textContent += button.textContent;
+        })
+    } 
+}
+
+pointButton.addEventListener('click', () => result.textContent += '.')
+
+deleteButton.addEventListener('click', () => {
+    result.textContent = '';
+})
+
+procentButton.addEventListener('click', () => {
+    a = +result.textContent;
+    result.textContent = `${procent()}`;
+})
+
+posNegButton.addEventListener('click', () => {
+    result.textContent = `${+result.textContent * -1}`;
+})
+
+divideButton.addEventListener('click', () => {
+    addEventListeners(divide);
+})
+
+multyButton.addEventListener('click', () => {
+    addEventListeners(multiply);
+})
+
+minButton.addEventListener('click', () => {
+    addEventListeners(subtract);
+})
+
+plusButton.addEventListener('click', () => {
+    addEventListeners(sum);
+})
+
+buttons.forEach(button => {
+    checkButtonNumber(button);
+})
